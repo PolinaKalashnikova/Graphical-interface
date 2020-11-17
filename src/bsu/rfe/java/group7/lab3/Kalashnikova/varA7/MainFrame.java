8655f4697e42c5788158f6b98532eee5e97f8ccb
PolinaKalashnikova <polina.kalasnnikova.2002@bk.ru>
2020-11-17 16:24:16 +0300

    Добавлен визуализатор GornerTableCellRenderer и его реализация

--- src/bsu/rfe/java/group7/lab3/Kalashnikova/varA7/MainFrame.java
+++ src/bsu/rfe/java/group7/lab3/Kalashnikova/varA7/MainFrame.java
@@ -40,6 +40,12 @@ public class MainFrame extends JFrame {
     private JTextField textFieldStep;
     private Box hBoxResult;
 
+    // Визуализатор ячеек таблицы
+
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    // Модель данных с результатами вычислений
+
     private GornerTableModel data;
 
     public MainFrame(Double[] coefficients) {
@@ -369,6 +375,12 @@ public class MainFrame extends JFrame {
 
                     JTable table = new JTable(data);
 
+                    // Установить в качестве визуализатора ячеек для класса Double разработанный визуализатор
+
+                    table.setDefaultRenderer(Double.class,
+
+                            renderer);
+
                     // Установить размер строки таблицы в 30 пикселов
 
                     table.setRowHeight(30);
@@ -526,6 +538,8 @@ public class MainFrame extends JFrame {
         }
 
     }
+
+
 
     protected void saveToTextFile(File selectedFile) {
 
